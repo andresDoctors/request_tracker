507447d62b3aa22aa9812d7ed43a50f9e47ad922
andresDoctors <101465588+andresDoctors@users.noreply.github.com>
2023-02-10 00:34:50 -0300

    changed arguments taken by some functions

--- app.js
+++ app.js
@@ -6,41 +6,44 @@ const https = require('https')
 const parse_url = (url) => {
 
   let arr = url.split('/')
-  let filename = arr.pop()
-
-  // filenames not support '?' char, so we replace it by '##' string
-  filename = filename.replace('?', '##')
-
+  arr.pop()
   arr.shift()
+
   let dir = './'
   for (let i = 0; i < arr.length; i++) {
     dir += arr[i] + '/';
   }
 
-  return {dir, filename}
+  return dir
 }
 
-const create_directories = (dir) => {
+const make_directories = (dir) => {
 
   fs.mkdirSync(dir, { recursive: true }, (err) => {
     if (err) throw err;
-  });
+  })
 }
 
-const dump_request = (dir, filename, req) => {
-
-  create_directories(dir)
+const dump_request = (req) => {
 
   const { rawHeaders, url } = req
   const mini_req = { rawHeaders, url }
 
+  const dir = parse_url(url)
+  make_directories(dir)
+
+  // filenames not support '?' char, so we replace it by '##' string
+  const write_path = '.' + url.replace('?', '##')
+
   fs.writeFileSync(
-    dir + filename + '.req',
-    stringify(mini_req, null, 2)
+    write_path + '.req',
+    JSON.stringify(mini_req, null, 2)
   )
+
+  return write_path
 }
 
-const download_response = (dir, filename, req, ress) => {
+const download_response = (write_path, req, ress) => {
 
   const options = {
     host: "www.rappi.com.ar",
@@ -54,13 +57,13 @@ const download_response = (dir, filename, req, ress) => {
 
     res.on("data", (chunk) => {
       console.log('http_data')
-      fs.writeFileSync(dir + filename + '.res', chunk, { flag: 'a' })
+      fs.writeFileSync(write_path + '.res', chunk, { flag: 'a' })
     })
 
     res.on("end", () => {
       console.log('http_end')
       req.end()
-      const readStream = fs.createReadStream(dir + filename + '.res')
+      const readStream = fs.createReadStream(write_path + '.res')
       readStream.pipe(ress)
     })
   })
@@ -73,9 +76,9 @@ const server = http.createServer((req, res) => {
   const {dir, filename} = parse_url(req.url)
   console.log(`dir: \"${dir}\"`, `filename: \"${filename}\"`) //debug
 
-  dump_request(dir, filename, req)
+  const write_path = dump_request(dir, filename, req)
 
-  download_response(dir, filename, req, res)
+  download_response(write_path, req, res)
 
 })
 server.listen(5000)
